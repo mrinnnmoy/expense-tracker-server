7930c16d622fd86f12df3c252d6ce3d191a83e63
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-07 20:39:15 +0530

    create database-connection

--- server.js
+++ server.js
@@ -2,16 +2,33 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-require('dotenv').config({path : "./config.env"});
+require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
 // Use middleware
 app.use(cors());
 app.use(express.json());
 
-// Using routes
-app.use(require('./routes/route'));
+// Mongodb Connection
+const con = require("./db/connection");
 
-app.listen(port, () => {
-  console.log(`Server is running on port: http://localhost:${port}`);
-});
+// Using routes
+app.use(require("./routes/route"));
+
+con
+  .then((db) => {
+    if (!db) return process.exit(1);
+
+    // listen to http server
+    app.listen(port, () => {
+      console.log(`Server is running on port: http://localhost:${port}`);
+    });
+
+    app.on("error", (err) =>
+      console.log(`Failed to connect with HTTP server : ${err}`)
+    );
+    //   error in mongodb connection
+  })
+  .catch((error) => {
+    console.log(`Connection Failed...! ${error}`);
+  });
